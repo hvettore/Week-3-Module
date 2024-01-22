9665066dde90be371160ebd7e9ed170476eaecc8
Hugo Vettore <vettore.h@gmail.com>
2024-01-22 21:31:48 +0100

    task 3 done

--- task3.cs
+++ task3.cs
@@ -1,22 +1,26 @@
-// Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
-Turn();
-Move();
-Move();
-Turn();
-Turn();
-Turn();
-Move();
-*/
-
+while(!AtGoal)
+{
+    CheckAndMove():
+}
 
 #region Basic functions
-// These functions are just her to make your intelisense work. 
-// They only have a conceptual function.
+
+void CheckAndMove()
+{
+    if (Peek())
+    {
+        Move();
+    }
+    else
+    {
+        Turn();
+        Peek();
+        if(!Peek())
+        {
+            FullTurn();
+        }
+    }
+}
 
 void Move()
 {
@@ -28,16 +32,21 @@ void Turn()
     // Turns the car 90 deg clockwise.
 }
 
+void FullTurn()
+{
+    // Turns the car 180 deg clockwise.
+}
+
 bool Peek()
 {
     // Returns true if the next cell is open, otherwise false.
-    return true; // Just a placeholder value. 
+    return true;
 }
 
 bool AtGoal()
 {
     // Returns true if the current cell is the goal cell.
-    return true; // just a placholder
+    return true;
 }
 
 #endregion
