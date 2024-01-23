a7e617c42e0ff67ed3e56bcc22a6485d8384aa38
Hugo Vettore <vettore.h@gmail.com>
2024-01-23 11:22:16 +0100

    task 5 complete

--- task5.cs
+++ task5.cs
@@ -1,22 +1,70 @@
-// Your program should start at this line.
-/*
-Example:
-Move();
-Move();
-Move();
-Turn();
-Move();
-Move();
-Turn();
-Turn();
-Turn();
-Move();
-*/
+bool Direction1;
+bool Direction2;
+bool Direction3;
+bool Direction4;
 
+while (!AtGoal)
+{
+    RouteCheck();
+    RouteAct();
+}
 
 #region Basic functions
 // These functions are just her to make your intelisense work. 
 // They only have a conceptual function.
+
+void RouteCheck()
+{
+    if (Peek())
+    {
+        Direction1 = true
+    }
+    Turn();
+    if (Peek())
+    {
+        Direction2 = true
+    }
+    Turn();
+    if (Peek())
+    {
+        Direction3 = true
+    }
+    Turn();
+    if (Peek())
+    {
+        Direction4 = true
+    }
+    Turn();
+}
+
+void RouteAct()
+{
+    if (Direction1 == true)
+    {
+        Move();
+    }
+    else if (Direction2 == true)
+    {
+        Turn();
+        Move();
+    }
+    else if (Direction1 == true)
+    {
+        FullTurn();
+        Move();
+    }
+    else if (Direction1 == true)
+    {
+        FullTurn();
+        Turn();
+        Move();
+    }
+
+    Direction1 = false
+    Direction2 = false
+    Direction3 = false
+    Direction4 = false
+}
 
 void Move()
 {
@@ -26,6 +74,11 @@ void Move()
 void Turn()
 {
     // Turns the car 90 deg clockwise.
+}
+
+void FullTurn()
+{
+    // Turns the car 180 deg clockwise.
 }
 
 bool Peek()
